f1f8505802c50f0590c3a7e1dbd3a513b7a99882
Slan14 <mahmood.naderi98@gmail.com>
2025-09-10 16:02:37 +0300

    Lab01: exercise for history of the commands

---
+++ Lab01/Exercises/ex2.sh
@@ -0,0 +1,15 @@
+# Run some random commands that you know or just use the following commands))
+
+touch grass
+ls
+mv grass noice
+rm noice
+whoami
+ls
+
+# if you already have this directory from exercise 1, do not uncomment following line.
+# mkdir ~/week01
+cd ~/week01
+
+# history -a # just to flush the history to the hist file
+history > ex2.txt
